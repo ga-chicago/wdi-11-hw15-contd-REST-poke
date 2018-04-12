366048e1caf606c37f52bc7ad3737451a03662bc
Tyler Earls <tyler.a.earls@gmail.com>
2018-04-11 21:40:17 -0500

    set up mvp server

--- controllers/PokemonController.js
+++ controllers/PokemonController.js
@@ -11,11 +11,18 @@ router.get('/', (req, res) => {
 	})
 })
 
+// *** new route ***
+
+router.get('/new', (req, res) => {
+	res.render('new.ejs') // rendering a template
+})
+
 // *** show route ***
 
 router.get('/:id', (req, res) => {
 	res.render('show.ejs', {
-		pokemon: Pokemon[req.params.id]
+		pokemon: Pokemon[req.params.id],
+		index: req.params.id
 	})
 })
 
@@ -28,16 +35,62 @@ router.get('/:id/edit', (req, res) => {
 	})
 })
 
-// *** new route ***
-
-router.get('/new', (req, res) => {
-	res.render('new.ejs') // rendering a template
-})
 
 // *** post route ***
 
+router.post('/', (req, res) => {
+
+	// now that we have body-parser the data is available to us in req.body
+	console.log(req.body);
+		const newPokemon = {
+			name: req.body.name,
+			type: [req.body.type1, req.body.type2],
+			hp: req.body.hp,
+			attack: req.body.attack,
+			defense: req.body.defense
+		}
+		console.log(newPokemon);
+	Pokemon.push(newPokemon)
+	// you can redirect the user in lieu of res.render or res.send
+	res.redirect('/pokemon')
+})
+
 // *** delete route ***
+
+router.delete('/:id', (req, res) => {
+	Pokemon.splice(req.params.id, 1);
+	res.redirect('/pokemon');
+})
 
 // *** put route ***
 
+router.put('/:id', (req, res) => {
+
+	// if (req.body.type2) {
+	// 	const editedPokemon = {
+	// 		name: req.body.name,
+	// 		type: [req.body.type1, req.body.type2],
+	// 		hp: req.body.hp,
+	// 		attack: req.body.attack,
+	// 		defense: req.body.defense
+	// 	}
+	// } else {
+		const editedPokemon = {
+			name: req.body.name,
+			type: [req.body.type1, req.body.type2],
+			stats: { 
+				hp: req.body.hp,
+				attack: req.body.attack,
+				defense: req.body.defense
+			},
+			img: Pokemon[req.params.id].img
+		}
+	// }
+
+	Pokemon[req.params.id] = editedPokemon;
+	// redirect to all the pokemon
+	res.redirect('/pokemon')
+})
+
+// EXPORT
 module.exports = router;
