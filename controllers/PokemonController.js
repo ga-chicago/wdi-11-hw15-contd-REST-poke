bb095f91297c13249e6463f8c338b2fdebd18be4
Tyler Earls <tyler.a.earls@gmail.com>
2018-04-11 20:13:59 -0500

    show view displays mvp info

--- controllers/PokemonController.js
+++ controllers/PokemonController.js
@@ -11,12 +11,33 @@ router.get('/', (req, res) => {
 	})
 })
 
+// *** show route ***
 
+router.get('/:id', (req, res) => {
+	res.render('show.ejs', {
+		pokemon: Pokemon[req.params.id]
+	})
+})
 
 // *** edit route ***
 
+router.get('/:id/edit', (req, res) => {
+	res.render('edit.ejs', {
+		pokemon: Pokemon[req.params.id],
+		index: req.params.id
+	})
+})
+
 // *** new route ***
 
-// *** show route ***
+router.get('/new', (req, res) => {
+	res.render('new.ejs') // rendering a template
+})
+
+// *** post route ***
+
+// *** delete route ***
+
+// *** put route ***
 
 module.exports = router;
