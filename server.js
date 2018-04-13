19337f3dddc5b1d3e6dc1d56b0d7733cdab05725
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-04-13 04:00:11 -0500

    Added Middleware

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
+//Require Express lib to use server
 const express = require('express');
+//instantiate app object
 const app = express();
+//port we will be using
 const port = 3000;
+//require the pokemon Controller with all the routes for /pokemon
 const pokemonController = require('./controllers/pokemonController');
+//Middleware we will need
+app.use((req,res,next)=>{
+	console.log('Middleware is working...');
+	next();
+})
+//We need middleware to parse data from POST request
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}))
+//We need middleware to allow static pages to function (CSS, Images, JS, etc...)
+app.use(express.static('public'));
+//We need middleware for PUT and DELETE methods
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
 
 
 
@@ -34,13 +51,11 @@ const pokemonController = require('./controllers/pokemonController');
 
 
 
+//Any request made to this route will use pokemonController
+app.use('/pokemon', pokemonController);
 
 
-
-
-
-
-
+//List for requests on port 3000
 app.listen(port, ()=>{
 	console.log(`Server is listening on port ${port} ...`);
 })
