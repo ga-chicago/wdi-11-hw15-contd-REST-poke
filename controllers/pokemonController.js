d74e996548d6d93651fbd1fc649b6188463141ca
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-04-13 04:35:55 -0500

    Added index route

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -1,11 +1,20 @@
+//We need Express lib to use router
 const express = require('express');
+//instantiate router from Router Method in express lib
 const router = express.Router();
 
-
+//require the pokemon.js in models that represents our database
 const pokemon = require('../models/pokemon');
 
 
 
+//Index Route with A list of Pokemon
+router.get('/', (req, res)=>{
+	res.render('index.ejs', {
+		heading: 'Welcome To The Pokemon Index!',
+		thePokemon: pokemon 
+	})
+});
 
 
 
@@ -45,7 +54,5 @@ const pokemon = require('../models/pokemon');
 
 
 
-
-
-
+//Export routers to any files that are requiring this file
 module.exports = router;
